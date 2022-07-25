20deef09c8b7797c42a4e13ca5c959c613cc02d6
Navid <navidved@gmail.com>
2022-07-26 00:17:15 +0430

    structure fix on schemas, routes, repo and auth2

--- app/router.py
+++ app/router.py
@@ -1,13 +1,13 @@
 from fastapi import FastAPI
-from app.core import root_router
-from app.blog.routers import post_router, user_router
+from app.core import root_routes
+from app.blog.routers import post_routes, user_routes
 
 
 
 class Router:
   def __init__(self, app: FastAPI):
-    app.include_router(root_router.router)
-    app.include_router(post_router.router)
-    app.include_router(user_router.router)
+    app.include_router(root_routes.router)
+    app.include_router(post_routes.router)
+    app.include_router(user_routes.router)
     
 
